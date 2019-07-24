be751ac3f8b9977dd130828048138c8cb681da4e
jce-11 <evansj39@uni.coventry.ac.uk>
2019-07-24 15:33:00 +0100

    model.py

--- research/attention_ocr/python/model.py
+++ research/attention_ocr/python/model.py
@@ -1,28 +1,4 @@
-# Copyright 2017 The TensorFlow Authors All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 
-"""Functions to build the Attention OCR model.
-
-Usage example:
-  ocr_model = model.Model(num_char_classes, seq_length, num_of_views)
-
-  data = ... # create namedtuple InputEndpoints
-  endpoints = model.create_base(data.images, data.labels_one_hot)
-  # endpoints.predicted_chars is a tensor with predicted character codes.
-  total_loss = model.create_loss(data, endpoints)
-"""
 import sys
 import collections
 import logging
